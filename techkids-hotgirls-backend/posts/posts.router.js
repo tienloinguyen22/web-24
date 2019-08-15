40a8c1ae11e95652648c45208becc9472e1ecbd3
sese22 <tienloinguyen22@gmail.com>
2019-08-15 23:21:44 +0700

    update pagination

--- techkids-hotgirls-backend/posts/posts.router.js
+++ techkids-hotgirls-backend/posts/posts.router.js
@@ -3,6 +3,56 @@ const PostsModel = require('./posts.model');
 
 const postRouter = express.Router();
 
+postRouter.get('/', (req, res) => {
+  // sortBy: price
+  // sortOrder: asc|desc
+  const pageNumber = Number(req.query.pageNumber);
+  const pageSize = Number(req.query.pageSize);
+
+  // validate
+  if (isNaN(pageNumber) || isNaN(pageSize)) {
+    res.status(500).json({
+      success: false,
+      message: 'pageNmber && pageSize is invalid'
+    });
+  }
+  if (pageNumber < 1 || pageSize < 1 || pageSize > 20) {
+    res.status(500).json({
+      success: false,
+      message: 'pageNmber && pageSize is invalid'
+    });
+  }
+
+  // query db | 1|-1
+  PostsModel.find({})
+    .sort({createdAt: -1})
+    .skip(pageSize * (pageNumber - 1))
+    .limit(pageSize)
+    .exec((error, data) => {
+      if (error) {
+        res.status(500).json({
+          success: false,
+          message: error.message
+        });
+      } else {
+        PostsModel.find({}).countDocuments().exec((err, total) => {
+          if (err) {
+            res.status(500).json({
+              success: false,
+              message: err.message
+            });
+          } else {
+            res.status(200).json({
+              success: true,
+              data: data,
+              total: total,
+            })
+          }
+        });
+      }
+    });
+});
+
 postRouter.post('/create', (req, res) => {
   // check user login ?
   if (req.session.currentUser && req.session.currentUser._id) {
--- techkids-hotgirls-frontend/src/pages/HomeScreen.jsx
+++ techkids-hotgirls-frontend/src/pages/HomeScreen.jsx
@@ -1,20 +1,115 @@
 import React from 'react';
 
 class HomeScreen extends React.Component {
+  state = {
+    pageNumber: 1,
+    pageSize: 3,
+    data: [],
+    total: 0,
+  };
+
   componentDidMount() {
-    fetch('http://localhost:3001/users/test', {
-      credentials: 'include',
+    // fetch posts
+    fetch(`http://localhost:3001/posts?pageNumber=${this.state.pageNumber}&pageSize=${this.state.pageSize}`, {
+      method: 'GET',
       headers: {
-        'Accept': 'application/json',
         'Content-Type': 'application/json',
       },
-      method: 'GET',
-    });
+      credentials: 'include',
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        this.setState({
+          data: data.data,
+          total: data.total,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
+  handlePaginationClick = (event) => {
+    console.log(event.target.innerText);
+    // set state page number
+    this.setState({
+      pageNumber: Number(event.target.innerText),
+    });
+
+    // fetch new data
+    fetch(`http://localhost:3001/posts?pageSize=${this.state.pageSize}&pageNumber=${event.target.innerText}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        this.setState({
+          data: data.data,
+          total: data.total,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   render() {
+    const myArray = [];
+    for (let i = 0; i < Math.ceil(this.state.total / this.state.pageSize); i += 1) {
+      myArray.push(i);
+    }
     return (
-      <div>Home screen</div>
+      <div>
+        <div className='result'>
+          {this.state.data.map((item) => {
+            return (
+              <div className="card mt-5" key={item._id}>
+                <div
+                  className="card-img-top"
+                  style={{
+                    backgroundImage: `url(${item.imageUrl})`,
+                    height: `300px`,
+                    backgroundRepeate: 'no-repeate',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center'
+                  }}
+                >
+                </div>
+                <div className="card-body">
+                  <p className="card-text">{item.content}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+
+        <nav className='mt-4' aria-label="Page navigation example">
+          <ul className="pagination">
+            <li className="page-item">
+              <a className="page-link" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+                <span className="sr-only">Previous</span>
+              </a>
+            </li>
+
+            {myArray.map((item) => {
+              return (
+                <li className={this.state.pageNumber === item + 1 ? "page-item active" : "page-item"}>
+                  <a className="page-link" onClick={this.handlePaginationClick}>{item + 1}</a>
+                </li>
+              );
+            })}
+
+            <li className="page-item">
+              <a className="page-link" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+                <span className="sr-only">Next</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     );
   }
 }
